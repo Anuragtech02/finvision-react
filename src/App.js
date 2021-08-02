df40258e5252ece6af4761541115ede7f5dde90f
Anurag Pal <apal895@gmail.com>
2021-08-02 20:45:10 +0530

    homepage gradients setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import styles from "./App.module.scss";
 import Button from "./components/Button/Button";
+import rightGradient from "./assets/gradients/right-gradient.svg";
+import leftGradient from "./assets/gradients/left-gradient.svg";
 
 const App = () => {
   return (
     <div className={styles.container}>
       <Button variant="solid">Upload</Button>
-      <div className={styles.gradientRight}></div>
+      <div className={styles.gradientRight}>
+        <img src={rightGradient} alt="finvision-gradient" />
+      </div>
+      <div className={styles.gradientLeft}>
+        <img src={leftGradient} alt="finvision-gradient" />
+      </div>
     </div>
   );
 };
