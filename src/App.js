d7b309e3be309b521a690a18f46043057d82232f
Anurag Pal <apal895@gmail.com>
2021-08-02 22:27:03 +0530

    cursor comonent done

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import styles from "./App.module.scss";
 import rightGradient from "./assets/gradients/right-gradient.svg";
 import leftGradient from "./assets/gradients/left-gradient.svg";
 import Navbar from "./components/Navbar/Navbar";
+import Cursor from "./components/Cursor/Cursor";
 
 const App = () => {
   return (
-    <div className={styles.container}>
-      <Navbar />
-      <div className={styles.gradientRight}>
-        <img src={rightGradient} alt="finvision-gradient" />
+    <>
+      <Cursor />
+      <div className={styles.container}>
+        <Navbar />
+        <div className={styles.gradientRight}>
+          <img src={rightGradient} alt="finvision-gradient" />
+        </div>
+        <div className={styles.gradientLeft}>
+          <img src={leftGradient} alt="finvision-gradient" />
+        </div>
       </div>
-      <div className={styles.gradientLeft}>
-        <img src={leftGradient} alt="finvision-gradient" />
-      </div>
-    </div>
+    </>
   );
 };
 
