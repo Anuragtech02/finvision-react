9d340d09ab89330e1009ca92f5bafd1520709946
shishirtiwari23 <shishir456tiwari@gmail.com>
2021-08-04 20:10:57 +0530

    Home Page Almost Complete

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -4,11 +4,16 @@ const Home = () => {
   return (
     <div className={styles.container}>
       <img className={styles.image} src={Coin} alt="" />
-      <h2>Lets Visualize</h2>
-      <h1>Finance</h1>
-      <p>
+      <h2 className={styles.sideHeading}>LETS VISUALIZE</h2>
+      <h1 className={styles.mainHeading}>FINANCE </h1>
+      <p className={styles.para}>
         Investing is an act of buying assets or items that help you generate
         income in the long run.
+        <br />
+        <br />
+        <span style={{ fontSize: "0.9rem", fontWeight: "300" }}>
+          &#8595; SCROLL TO DISCOVER
+        </span>
       </p>
     </div>
   );
